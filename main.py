54a91f3afb5824460977b14519de4c930d09e388
as-tari <astari915@gmail.com>
2024-12-03 05:55:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,8 @@ def logout():
 
         
 # Menampilkan logo aplikasi
-st.logo("static/images/logo2.png", icon_image="static/images/5.png")
+st.image("static/images/3.png")
+st.logo("static/images/3.png", icon_image="static/images/logo2.png")
 
 # Menampilkan halaman login jika belum login
 if st.session_state.role is None:
