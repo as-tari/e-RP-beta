e76799b276c9cfdcff63e68002b5c483b85b2cf5
as-tari <astari915@gmail.com>
2024-12-03 02:18:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from utils.auth import check_role
 from admin.dashboard import show_dashboard as admin_dashboard
-from coordinator.dashboard import show_dashboard as coordinator_dashboard
+from team.coordinator import show_dashboard as team_coordinator_dashboard
 from team.navy import show_dashboard as navy_dashboard
 from team.niki import show_dashboard as niki_dashboard
 from team.eta import show_dashboard as eta_dashboard
+from team.tari import show_dashboard as tari_dashboard
 from lecturer.dashboard import show_dashboard as lecturer_dashboard
 from student.dashboard import show_dashboard as student_dashboard
 
@@ -16,16 +17,18 @@ if "role" not in st.session_state:
 def display_dashboard():
     if st.session_state.role == "Admin":
         admin_dashboard()
-    elif st.session_state.role == "Coordinator":
-        coordinator_dashboard()
-    elif st.session_state.role == "Eta":
-        navy_dashboard()
-    elif st.session_state.role == "Navy":
-        niki_dashboard()
-    elif st.session_state.role == "Niki":
-        eta_dashboard()
-    elif st.session_state.role == "Tari":
-        eta_dashboard()      
+    elif st.session_state.role == "Team":
+        team_role = st.selectbox("Select your team role", ["Coordinator", "Navy", "Niki", "Eta", "Tari"])
+        if team_role == "Coordinator":
+            team_coordinator_dashboard()
+        elif team_role == "Navy":
+            navy_dashboard()
+        elif team_role == "Niki":
+            niki_dashboard()
+        elif team_role == "Eta":
+            eta_dashboard()
+        elif team_role == "Tari":
+            tari_dashboard()
     elif st.session_state.role == "Lecturer":
         lecturer_dashboard()
     elif st.session_state.role == "Student":
@@ -34,8 +37,9 @@ def display_dashboard():
         st.warning("Please log in to access your dashboard.")
 
 # Login dan pemilihan role
-st.title("📑 e-RP Assistant System")
-role_input = st.selectbox("Select your role", ["Admin", "Coordinator", "Eta", "Navy", "Niki", "Tari", "Lecturer", "Student"])
+st.title("Selamat Datang di Sistem Bantuan e-RP!")
+st.markdown("<h2 style='font-family: 'Courier New'; color: blue;'>Welcome!</h2>", unsafe_allow_html=True)
+role_input = st.selectbox("Select your role", ["Admin", "Team", "Lecturer", "Student"])
 st.session_state.role = role_input
 
 display_dashboard()
