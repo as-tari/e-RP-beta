8f8b09331dc880f7d0ac4e238fdc27b16d006aa6
as-tari <astari915@gmail.com>
2024-12-03 02:48:38 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ API_KEYS = {
     "example_key": "your_api_key_here"
 }
 
-ROLES = ["Admin", "Coordinator", "Eta", "Navy", "Niki", "Tari", "Lecturer", "Student"]
+ROLES = ["Admin", "Team", "Lecturer", "Student"]
+TEAM_ROLES = ["Coordinator", "Eta", "Navy", "Niki", "Tari"]
