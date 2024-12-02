cc84e4a795f640a56cda871989e934afea544c58
as-tari <astari915@gmail.com>
2024-12-03 02:45:11 +0700

    Update dashboard.py

--- admin/dashboard.py
+++ admin/dashboard.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 def show_dashboard():
-    st.title("Admin Dashboard")
+    st.title("{st.session_state.role} Dashboard")
     st.write(f"You are logged in as {st.session_state.role}.")
