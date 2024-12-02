5e9401489b2126c18a6f16776b0bf599a81ba834
as-tari <astari915@gmail.com>
2024-12-03 02:37:18 +0700

    Update coordinator.py

--- team/coordinator.py
+++ team/coordinator.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 def show_dashboard():
-    st.title("Coordinator Team Dashboard")
-    st.write("Welcome to the Coordinator Team Dashboard.")
+    st.title("Coordinator Dashboard")
+    st.write("You are logged in as Coordinator")
